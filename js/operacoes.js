e6053f84ac2996939d1ea2a38bf3757b2019c0c6
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-10-01 19:13:55 -0300

    Refatoração do algorítmo de montar as operações, funcionando

--- js/operacoes.js
+++ js/operacoes.js
@@ -2,72 +2,101 @@ let buttonSum = document.querySelector("#sum");
 let buttonSub = document.querySelector("#sub");
 let buttonMulti = document.querySelector("#multi");
 let buttonDivi = document.querySelector("#divi");
+let numberInput = document.querySelector("#input-num");
 
-let arrNumbers = []
+let arrNumbers = [];
+let eraseBeforeNextInput = false;
+let selectedNumber = 0;
 
-let total = 0;
-
-clearOperation();
-checkFirstNumber();
 sumOperation();
-
+subOperation();
+multiOperation();
+diviOperation();
 
 function sumOperation() {
     buttonSum.addEventListener("click", function() {
-        //console.log("tamanho " + arrNumbers.length);
-        let numberInput = document.querySelector("#input-num");
-        if (arrNumbers.length == 1 ) {
-            arrNumbers.push("+");
-            /* arrNumbers.forEach(function(n) {
-                console.log(n);
-            }) */
-        } else if (arrNumbers.length >= 2 ) {
-            arrNumbers.push(saveNumber(numberInput));
-            arrNumbers.push("+");
-            /* arrNumbers.forEach(function(n) {
-                console.log(n);
-            }) */        
-        }        
-    })
-}
-
-function checkFirstNumber() {
-    let ops = document.querySelectorAll(".button-op");
-    ops.forEach(function(operation) {
-        operation.addEventListener("click", function() {    
-            let numberInput = document.querySelector("#input-num");    
-            
-            if (checkInputEmpty(numberInput)) {
-                console.log("vazio");
-                return;        
-            } 
-            if (arrNumbers.length == 0) {
-                arrNumbers[0] = saveNumber(numberInput);
-                console.log(`first ${arrNumbers[0]}`);
-                clearInput();
-            }
-        })
-    })
-}
-
-function saveNumber(number) {
-    return number.value;    
-}
-
-function clearInput() {
-    let numberInput = document.querySelector("#input-num");
-    numberInput.value = '';    
-}
-
-function checkInputEmpty(input) {
-    return input.value == '';
-}
-
-function clearOperation() {
-    let clear = document.querySelector("#button-clear");
-    clear.addEventListener("click", function() {
-        arrNumbers = [];
-        total = 0;        
-        clearInput();
+        operationConstructor("+");        
     })    
+}
+
+function subOperation() {
+    buttonSub.addEventListener("click", function() {
+        operationConstructor("-");       
+    })    
+}
+
+function multiOperation() {
+    buttonMulti.addEventListener("click", function() {
+        operationConstructor("*");        
+    })    
+}
+
+function diviOperation() {
+    buttonDivi.addEventListener("click", function() {        
+        operationConstructor("/");
+    })    
+}
+
+function operationConstructor(operator) {
+    let pre = document.querySelector("#preview");
+        if (checkOperationInvalid(pre)) return;        
+        if (arrNumbers.length == 0) {
+            addSelectedNumber(pre);
+            addOperator(operator)
+            eraseStoredNumber(pre);
+        } else if (arrNumbers.length == 1){
+            addOperator(operator);
+            clearInput();
+            eraseBeforeNextInput = false;
+        } else {
+            if (pre.textContent == "") {
+                console.log("Entrei naquele erro maldito");
+                return;
+            }
+            addSelectedNumber(pre);
+            finishOperation();
+            addOperator(operator);
+            eraseStoredNumber(pre);
+        }
+}
+
+function addSelectedNumber(preview) { 
+    selectedNumber = preview.textContent;
+    arrNumbers.push(selectedNumber);        
+}
+
+function addOperator(operation) {
+    arrNumbers.push(operation);
+}
+
+function finishOperation() {
+    let calculatedNumbers = getResultado();            
+    numberInput.value = calculatedNumbers;
+    arrNumbers = [];            
+    arrNumbers.push(calculatedNumbers);
+}
+
+function eraseStoredNumber(preview) {
+    preview.textContent = "";
+}
+
+function checkOperationInvalid(preview) {
+    console.log(preview.textContent === "" && numberInput.value === "");
+    return preview.textContent === "" && numberInput.value === "";
+    
+}
+
+function getResultado() {    
+    let r = eval(arrNumbers.join(""));
+    if (r == "Infinity"){
+        return 0;
+    } else {
+        return r;
+    }    
+}
+
+function clearInput() {    
+    let pre = document.querySelector("#preview");    
+    pre.textContent = '';    
+    numberInput.value = '';    
 }
