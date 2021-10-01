5c8837ccc3859039a5a8d6f9b0419a11a331c320
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-10-01 18:58:46 -0300

    Vincular números aos botões refatorado

--- js/vincular-numeros.js
+++ js/vincular-numeros.js
@@ -1,10 +1,21 @@
-function vinculateNumberButton() {
-    let result = document.querySelector("#input-num");
+function vinculateNumberButton() {    
     let buttons = document.querySelectorAll(".button-num");
+    let pre = document.querySelector("#preview");
 
     buttons.forEach(function(button) {
         button.addEventListener("click", function() {
-            result.value += button.textContent;
+            if (eraseBeforeNextInput) {
+                console.log("before" + arrNumbers)
+                clearInput();
+                arrNumbers = [];
+                pre.textContent += button.textContent;
+                eraseBeforeNextInput = false;
+                console.log("after" + arrNumbers)
+            } else {
+                pre.textContent += button.textContent;
+            }
+            
+            
         })
     })
 }
