f46bd93f331fce1478db1db78287db0268d415e2
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-10-04 16:47:07 -0300

    Refatoração do cálculo das operações e comentários

--- js/vincular-numeros.js
+++ js/vincular-numeros.js
@@ -1,21 +1,18 @@
+// Vincula cada botão com seus respectivos números e salva o valor
 function vinculateNumberButton() {    
     let buttons = document.querySelectorAll(".button-num");
     let pre = document.querySelector("#preview");
 
     buttons.forEach(function(button) {
         button.addEventListener("click", function() {
-            if (eraseBeforeNextInput) {
-                console.log("before" + arrNumbers)
+            if (eraseBeforeNextInput) { //checa se o resultado deve ser apagado antes do input do usuário                
                 clearInput();
-                arrNumbers = [];
-                pre.textContent += button.textContent;
-                eraseBeforeNextInput = false;
-                console.log("after" + arrNumbers)
+                emptyArray(); //reinicia o array de operações                
+                pre.textContent += button.textContent; 
+                eraseBeforeNextInput = false; //depois que o usuário interagiu uma vez, não apaga mais
             } else {
-                pre.textContent += button.textContent;
+                pre.textContent += button.textContent; //insere o valor do botão continuamente
             }
-            
-            
         })
     })
 }
